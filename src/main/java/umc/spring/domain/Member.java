32defae338162c1ac7bd9e0d812b662c35f3a279
kwon2059 <127478451+kwon2059@users.noreply.github.com>
2023-12-20 23:03:25 +0900

    initialize
    initialize

--- src/main/java/umc/spring/domain/Member.java
+++ src/main/java/umc/spring/domain/Member.java
@@ -2,6 +2,8 @@ package umc.spring.domain;
 
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 import umc.spring.domain.common.BaseEntity;
 import umc.spring.domain.common.enums.Gender;
 import umc.spring.domain.common.enums.MemberStatus;
@@ -15,8 +17,20 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
+@DynamicUpdate
+@DynamicInsert
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -29,13 +43,6 @@ public class Member extends BaseEntity {
     @Column(nullable = false, length = 20)
     private String name;
 
-    @Enumerated(EnumType.STRING)
-    @Column(columnDefinition = "VARCHAR(10)")
-    private Gender gender;
-
-    @Column(nullable = false)
-    private Integer age;
-
     @Column(nullable = false, length = 40)
     private String address;
 
@@ -43,34 +50,33 @@ public class Member extends BaseEntity {
     private String specAddress;
 
     @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(10)")
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Enumerated(EnumType.STRING)
     @Column(columnDefinition = "VARCHAR(15) DEFAULT 'ACTIVE'")
-    //@Column(nullable = false, length = 15)
-    //@ColumnDefault("'ACTIVE'")
     private MemberStatus status;
 
     private LocalDate inactiveDate;
 
-    @Enumerated(EnumType.STRING)
-    @Column(columnDefinition = "VARCHAR(10)")
-    private SocialType socialType;
-
-    @Column(nullable = false, length = 50)
+    //    @Column(nullable = false, length = 50)
     private String email;
 
     @ColumnDefault("0")
     private Integer point;
 
-    //MemberPrefer과 양방향 매핑 1:N = Member : Mb_prefer
-    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
-    private List<MemberPrefer> memberPreferList = new ArrayList<>();
-    //MemberAgree과 양방향 매핑
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<MemberAgree> memberAgreeList = new ArrayList<>();
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<MemberPrefer> memberPreferList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<Review> reviewList = new ArrayList<>();
 
-    //MemberMission과 양방향 매핑
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<MemberMission> memberMissionList = new ArrayList<>();
 }
--- src/main/java/umc/spring/web/dto/MemberRequestDTO.java
+++ src/main/java/umc/spring/web/dto/MemberRequestDTO.java
@@ -22,11 +22,9 @@ public class MemberRequestDTO {
         Integer birthMonth;
         @NotNull
         Integer birthDay;
-        @Size(min = 6, max = 127)
-        String email;
-        @Size(min = 5, max = 12)
+        @NotNull
         String address;
-        @Size(min = 5, max = 12)
+        @NotNull
         String specAddress;
         @ExistCategories
         List<Long> preferCategory;
